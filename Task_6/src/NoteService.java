5583d5e1801ae985a4c90ec41e76976902e053d4
kvladimirovich10 <kvladimirovich10@gmail.com>
2017-09-28 21:11:48 +0300

    Create Main class

--- Task_6/src/NoteService.java
+++ Task_6/src/NoteService.java
@@ -1,1 +1,1 @@
-public class NoteService {}
+public class NoteService {    void addContact(Note note,                    String name,                    String lastname,                    String phoneNumber,                    String email) {        Contact contact = new Contact(name, lastname, phoneNumber, email);        note.noteContacts.add(contact);    }}
--- Task_6/src/NotebookService.java
+++ Task_6/src/NotebookService.java
@@ -1,1 +1,1 @@
-public class NotebookService {}
+public class NotebookService {}
